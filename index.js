3625195269ffe98e9c940a27d2b0ca19c326f2db
Macdeo <akinbiyi.adeola@gmail.com>
2023-03-25 23:55:24 +0100

    modified:   index.js 	modified:   styles.css

--- index.js
+++ index.js
@@ -3,8 +3,31 @@ var numberOfDrumButton = document.querySelectorAll('.drum').length;
 
 for(var i = 0; i < numberOfDrumButton; i++){
     document.querySelectorAll('button')[i].addEventListener('click', function (){
-        alert('I got Clicked');
+        // alert('I got Clicked');
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
     });
     
 }
 
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+
+// function multiply(num1, num2){
+//     return num1 * num2;
+// }
+
+// function substract(num1, num2){
+//     return num1 - num2;
+// }
+
+// function divided(num1, num2){
+//     return num1 / num2;
+// }
+
+// function operator(num1, num2, operator){
+//     return operator(num1, num2);
+// }
+
+// console.log(operator(2,3, divided));
